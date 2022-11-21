99e3addba98092d7120ea039cd2a73119a38b9d5
grupo 2 ciclo 4 <112993838+efrenorjuela@users.noreply.github.com>
2022-11-21 00:18:26 -0500

    Add files via upload

--- definitions/index.js
+++ definitions/index.js
@@ -1,87 +1,44 @@
 "use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-Object.defineProperty(exports, "ALIAS_KEYS", {
-  enumerable: true,
-  get: function () {
-    return _utils.ALIAS_KEYS;
-  }
-});
-Object.defineProperty(exports, "BUILDER_KEYS", {
-  enumerable: true,
-  get: function () {
-    return _utils.BUILDER_KEYS;
-  }
-});
-Object.defineProperty(exports, "DEPRECATED_KEYS", {
-  enumerable: true,
-  get: function () {
-    return _utils.DEPRECATED_KEYS;
-  }
-});
-Object.defineProperty(exports, "FLIPPED_ALIAS_KEYS", {
-  enumerable: true,
-  get: function () {
-    return _utils.FLIPPED_ALIAS_KEYS;
-  }
-});
-Object.defineProperty(exports, "NODE_FIELDS", {
-  enumerable: true,
-  get: function () {
-    return _utils.NODE_FIELDS;
-  }
-});
-Object.defineProperty(exports, "NODE_PARENT_VALIDATIONS", {
-  enumerable: true,
-  get: function () {
-    return _utils.NODE_PARENT_VALIDATIONS;
-  }
-});
-Object.defineProperty(exports, "PLACEHOLDERS", {
-  enumerable: true,
-  get: function () {
-    return _placeholders.PLACEHOLDERS;
-  }
-});
-Object.defineProperty(exports, "PLACEHOLDERS_ALIAS", {
-  enumerable: true,
-  get: function () {
-    return _placeholders.PLACEHOLDERS_ALIAS;
-  }
-});
-Object.defineProperty(exports, "PLACEHOLDERS_FLIPPED_ALIAS", {
-  enumerable: true,
-  get: function () {
-    return _placeholders.PLACEHOLDERS_FLIPPED_ALIAS;
-  }
-});
-exports.TYPES = void 0;
-Object.defineProperty(exports, "VISITOR_KEYS", {
-  enumerable: true,
-  get: function () {
-    return _utils.VISITOR_KEYS;
-  }
-});
-var _toFastProperties = require("to-fast-properties");
-require("./core");
-require("./flow");
-require("./jsx");
-require("./misc");
-require("./experimental");
-require("./typescript");
-var _utils = require("./utils");
-var _placeholders = require("./placeholders");
-_toFastProperties(_utils.VISITOR_KEYS);
-_toFastProperties(_utils.ALIAS_KEYS);
-_toFastProperties(_utils.FLIPPED_ALIAS_KEYS);
-_toFastProperties(_utils.NODE_FIELDS);
-_toFastProperties(_utils.BUILDER_KEYS);
-_toFastProperties(_utils.DEPRECATED_KEYS);
-_toFastProperties(_placeholders.PLACEHOLDERS_ALIAS);
-_toFastProperties(_placeholders.PLACEHOLDERS_FLIPPED_ALIAS);
-const TYPES = [].concat(Object.keys(_utils.VISITOR_KEYS), Object.keys(_utils.FLIPPED_ALIAS_KEYS), Object.keys(_utils.DEPRECATED_KEYS));
-exports.TYPES = TYPES;
-
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const typeof_1 = __importDefault(require("./typeof"));
+const instanceof_1 = __importDefault(require("./instanceof"));
+const range_1 = __importDefault(require("./range"));
+const exclusiveRange_1 = __importDefault(require("./exclusiveRange"));
+const regexp_1 = __importDefault(require("./regexp"));
+const transform_1 = __importDefault(require("./transform"));
+const uniqueItemProperties_1 = __importDefault(require("./uniqueItemProperties"));
+const allRequired_1 = __importDefault(require("./allRequired"));
+const anyRequired_1 = __importDefault(require("./anyRequired"));
+const oneRequired_1 = __importDefault(require("./oneRequired"));
+const patternRequired_1 = __importDefault(require("./patternRequired"));
+const prohibited_1 = __importDefault(require("./prohibited"));
+const deepProperties_1 = __importDefault(require("./deepProperties"));
+const deepRequired_1 = __importDefault(require("./deepRequired"));
+const dynamicDefaults_1 = __importDefault(require("./dynamicDefaults"));
+const select_1 = __importDefault(require("./select"));
+const definitions = [
+    typeof_1.default,
+    instanceof_1.default,
+    range_1.default,
+    exclusiveRange_1.default,
+    regexp_1.default,
+    transform_1.default,
+    uniqueItemProperties_1.default,
+    allRequired_1.default,
+    anyRequired_1.default,
+    oneRequired_1.default,
+    patternRequired_1.default,
+    prohibited_1.default,
+    deepProperties_1.default,
+    deepRequired_1.default,
+    dynamicDefaults_1.default,
+];
+function ajvKeywords(opts) {
+    return definitions.map((d) => d(opts)).concat((0, select_1.default)(opts));
+}
+exports.default = ajvKeywords;
+module.exports = ajvKeywords;
 //# sourceMappingURL=index.js.map
